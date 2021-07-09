0988f88d9151731f3cf34aa08c1896e3179c383b
Pascal Herms <pherms@outlook.com>
2021-07-09 11:54:48 +0200

    First commit

---
+++ webapp/src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Navigation } from "./navigation";
+export { default as Footer } from "./footer";
+export { default as Home } from "./home";
+export { default as About } from "./about";
+export { default as Contact } from "./contact";
