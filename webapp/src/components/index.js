c27daf01000820acf3bafece8a4dddc2c7a3386b
Pascal Herms <pherms@outlook.com>
2021-08-05 11:29:57 +0200

    updates

--- webapp/src/components/index.js
+++ webapp/src/components/index.js
@@ -3,3 +3,14 @@ export { default as Footer } from "./footer";
 export { default as Home } from "./home";
 export { default as About } from "./about";
 export { default as Contact } from "./contact";
+
+function test() {
+    if(performance.memory.totalJSHeapSize == 0){
+        console.warn('totalJSHeapSize == 0');
+      }
+    
+      var bla = document.getELementByID(bla);
+      if(bla == '2'){
+        return bla;
+      }
+}
