d38e00fbf3de9f18d36cf88bede4d86aa3acdf33
Ubuntu <ubuntu@good-midge.merel107.local>
2021-08-06 12:27:45 +0000

    Updated docker install script

--- exercise1/install-docker/install-docker.sh
+++ exercise1/install-docker/install-docker.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
 # Update sources and apps
-apt update && apt upgrade
+apt update -y && apt upgrade -y
 
 # Remove docker if it is already installed
-apt-get remove docker docker-engine docker.io containerd runc
+apt remove -y docker docker-engine docker.io containerd runc
 
 # Install docker pre-requisites
-apt install curl apt-transport-https ca-certificates curl gnupg lsb-release
+apt install -y curl apt-transport-https ca-certificates curl gnupg lsb-release
 
 # Install docker engine, using the install script
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh get-docker.sh
+
+# add user to docker group
+if [ "$(lsb_release -is)" == "Ubuntu" ]; then
+  usermod -aG docker ubuntu
+  exec su -l $USER
+else
+  echo "Add user to \"docker\" group."
+fi
